8630ae223b159de85f275cc647bf76f37a85e530
1660157_TruongDinhHai <haitruong1405@gmail.com>
2019-06-08 15:44:06 +0700

    server

---
+++ server/server.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Hapi = require('@hapi/hapi')
+const mongoose = require('mongoose')
+
+const server = new Hapi.Server({
+  host: 'localhost',
+  port: 5000,
+  routes: {
+    cors: true
+  }
+})
+
+server.app.db = mongoose.connect(
+  'mongodb://localhost/hapitest',
+  {useNewUrlParser: true}
+)
+
+const init = async() =>{
+  await server
+  .register(
+    {plugin: require('./routes/tasks')},
+    {
+      routes: {
+        prefix: '/api'
+      }
+    }
+  )
+  .catch(err=>{
+    console.log(err);
+  })
+  await server.start()
+  console.log(`Server running at: ${server.info.uri}`)
+  // console.log("started");
+}
+
+init() 
